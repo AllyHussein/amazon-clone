de94d03a6c1238b863911cb0cbb5dd8884ff9f4e
AllyHussein <alihussein95@outlook.com>
2025-04-12 16:02:01 +0200

    create layout

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Amazon Clone";
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || "Spend Less, enjoy More";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "An Amazon Clone built with Next.js And MongoDB";
