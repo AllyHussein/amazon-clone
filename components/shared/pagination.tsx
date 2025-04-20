0daa41d16b40905ed8ccb0e188ffa6bbe6be4043
AllyHussein <alihussein95@outlook.com>
2025-04-20 17:49:37 +0200

    admin-orders

--- components/shared/pagination.tsx
+++ components/shared/pagination.tsx
@@ -6,6 +6,7 @@ import React from "react";
 import { formUrlQuery } from "@/lib/utils";
 
 import { Button } from "../ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 type PaginationProps = {
   page: number | string;
@@ -29,7 +30,7 @@ const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
     router.push(newUrl, { scroll: true });
   };
   return (
-    <div className="flex gap-2">
+    <div className="flex items-center gap-2">
       <Button
         size="lg"
         variant="outline"
@@ -37,8 +38,9 @@ const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
         onClick={() => onClick("prev")}
         disabled={Number(page) <= 1}
       >
-        Previous
+        <ChevronLeft /> Previous
       </Button>
+      Page {page} of {totalPages}
       <Button
         size="lg"
         variant="outline"
@@ -46,7 +48,7 @@ const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
         onClick={() => onClick("next")}
         disabled={Number(page) >= totalPages}
       >
-        Next
+        Next <ChevronRight />
       </Button>
     </div>
   );
